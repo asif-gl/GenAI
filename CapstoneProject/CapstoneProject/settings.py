dd9de667216b1c93278883b8e045fd3dee92bf21
Asif <mohammed.khan2@globallogic.com>
2023-10-19 14:19:17 +0530

    cors added

--- CapstoneProject/CapstoneProject/settings.py
+++ CapstoneProject/CapstoneProject/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'ChatBotApp',
+    'corsheaders',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = [    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -51,6 +53,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
+]
 ROOT_URLCONF = 'CapstoneProject.urls'
 
 TEMPLATES = [
